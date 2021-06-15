a930445e12be8d904134f99eb3b33f1915359498
Neven Jakopčić <neven.jakopcic@biss.hr>
2021-06-15 23:42:10 +0200

    Add iteration through excel file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import tkinter as tk
 from tkinter import filedialog, messagebox
@@ -7,25 +8,38 @@ from openpyxl import load_workbook
 
 
 def upload():
-    filename = filedialog.askopenfilename()
-    global ws
-    if filename.endswith('.xlsx'):
-        ws = load_workbook(filename).active
+    path = filedialog.askopenfilename()
+    global ws, filename
+    if path.endswith('.xlsx'):
+        ws = load_workbook(path).active
+        filename = get_file_name(path)
     else:
         messagebox.showerror('Error', 'Please select an .xlsx file.')
 
 
+def get_file_name(path):
+    return os.path.basename(path).split('.')[0]
+
+
 def generate():
-    global ws
+    global ws, filename
     deck = genanki.Deck(int(time.time()), 'Test deck')
     if ws:
-        deck.add_note(genanki.Note(model=anki_model, fields=[ws['D6'].value, ws['E6'].value]))
-        genanki.Package(deck).write_to_file('output.apkg')
-        messagebox.showinfo('Success', 'Successfully generated file')
+        row = 1
+        while ws.cell(row=row, column=1).value:
+            deck.add_note(genanki.Note(
+                model=anki_model,
+                fields=[
+                    ws.cell(row=row, column=1).value,
+                    ws.cell(row=row, column=2).value]))
+            row = row + 1
+
+        genanki.Package(deck).write_to_file(os.path.join(os.path.expanduser("~/Desktop"), f'{filename}.apkg'))
+        messagebox.showinfo('Success', 'Successfully generated file.')
+    else:
+        messagebox.showerror('Error', 'Please select a file.')
 
 
-root = tk.Tk()
-ws = None
 anki_model = genanki.Model(
     int(time.time()),
     'Basic Model',
@@ -36,7 +50,10 @@ anki_model = genanki.Model(
         'name': 'Card',
         'qfmt': '{{Question}}',
         'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'}])
-upload_button = tk.Button(root, text='Open', command=upload).pack()
-generate_button = tk.Button(root, text='Generate', command=generate).pack()
+ws = None
+filename = 'output'
+root = tk.Tk()
+tk.Button(root, text='Open', command=upload).grid(row=2, column=0, sticky=tk.W, pady=4)
+tk.Button(root, text='Generate', command=generate).grid(row=2, column=1, sticky=tk.W, pady=4)
 
 root.mainloop()
